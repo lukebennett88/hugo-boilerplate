8d105db05f762c30cdaa6436b5d42cce251ff59b
Luke Bennett <luke.bennett88@gmail.com>
2018-07-31 07:51:13 +1000

    Try to fix build not working

--- prod.sh
+++ prod.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-HUGO_ENV="production" hugo -d public --config config.toml,config_public.toml
+HUGO_ENV="production" hugo -d build/public --config config.toml,netlify.toml
 
-html-minifier --input-dir public --output-dir public --file-ext html --collapse-whitespace
+html-minifier --input-dir build/public --output-dir build/public --file-ext html --collapse-whitespace
+
+html-minifier --input-dir build/public --output-dir build/public --file-ext xml --collapse-whitespace

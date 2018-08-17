35de790649681e70c9948a74095501a77d8c90e4
Luke Bennett <luke.bennett88@gmail.com>
2018-08-18 09:41:41 +1000

    Update to newest version of Hugo

--- prod.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-hugo
-
-html-minifier --input-dir public --output-dir public --file-ext html --collapse-whitespace
-
-html-minifier --input-dir public --output-dir public --file-ext xml --collapse-whitespace

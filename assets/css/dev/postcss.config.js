ffbd135cfcbcb6a4725693b892f6ae3fc7e45cc9
Luke Bennett <luke.bennett88@gmail.com>
2018-10-27 16:42:53 +1100

    Fix PostCSS

--- assets/css/dev/postcss.config.js
+++ assets/css/dev/postcss.config.js
@@ -4,21 +4,23 @@ class TailwindExtractor {
   }
 }
 
-export const plugins = [
-  require('postcss-import')({
-    path: ['assets/css']
-  }),
-  require('tailwindcss')('./assets/css/tailwind.js'),
-  require('autoprefixer')({
-    browsers: [
-      'Android 2.3',
-      'Android >= 4',
-      'Chrome >= 20',
-      'Firefox >= 24',
-      'Explorer >= 8',
-      'iOS >= 6',
-      'Opera >= 12',
-      'Safari >= 6',
-    ],
-  }),
-];
+module.exports = {
+  plugins: [
+    require('postcss-import')({
+      path: ['assets/css']
+    }),
+    require('tailwindcss')('./assets/css/tailwind.js'),
+    require('autoprefixer')({
+      browsers: [
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 8',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6',
+      ],
+    }),
+  ],
+};
--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -4,33 +4,35 @@ class TailwindExtractor {
   }
 }
 
-export const plugins = [
-  require('postcss-import')({
-    path: ['assets/css']
-  }),
-  require('tailwindcss')('./assets/css/tailwind.js'),
-  require('@fullhuman/postcss-purgecss')({
-    content: ['layouts/**/*.html'],
-    extractors: [
-      {
-        extractor: TailwindExtractor,
-        extensions: ['html']
-      },
-    ],
-    fontFace: true,
-    whitelist: ['class1', 'class2']
-  }),
-  require('autoprefixer')({
-    grid: true,
-    browsers: [
-      'Android 2.3',
-      'Android >= 4',
-      'Chrome >= 20',
-      'Firefox >= 24',
-      'Explorer >= 8',
-      'iOS >= 6',
-      'Opera >= 12',
-      'Safari >= 6',
-    ],
-  }),
-];
+module.exports = {
+  plugins: [
+    require('postcss-import')({
+      path: ['assets/css']
+    }),
+    require('tailwindcss')('./assets/css/tailwind.js'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['layouts/**/*.html'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html']
+        },
+      ],
+      fontFace: true,
+      whitelist: ['class1', 'class2']
+    }),
+    require('autoprefixer')({
+      grid: true,
+      browsers: [
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 8',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6',
+      ],
+    }),
+  ],
+};

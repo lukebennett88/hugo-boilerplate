f4ea87ab98f2fc9a224a2d750596d5f3692cb0e1
Luke Bennett <luke.bennett88@gmail.com>
2018-11-13 21:00:41 +1100

    Linting

--- assets/css/dev/postcss.config.js
+++ assets/css/dev/postcss.config.js
@@ -6,21 +6,21 @@ class TailwindExtractor {
 
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      path: ['assets/css']
+    require("postcss-import")({
+      path: ["assets/css"]
     }),
-    require('tailwindcss')('./assets/css/tailwind.js'),
-    require('autoprefixer')({
+    require("tailwindcss")("./assets/css/tailwind.js"),
+    require("autoprefixer")({
       browsers: [
-        'Android 2.3',
-        'Android >= 4',
-        'Chrome >= 20',
-        'Firefox >= 24',
-        'Explorer >= 8',
-        'iOS >= 6',
-        'Opera >= 12',
-        'Safari >= 6',
-      ],
-    }),
-  ],
+        "Android 2.3",
+        "Android >= 4",
+        "Chrome >= 20",
+        "Firefox >= 24",
+        "Explorer >= 8",
+        "iOS >= 6",
+        "Opera >= 12",
+        "Safari >= 6"
+      ]
+    })
+  ]
 };
